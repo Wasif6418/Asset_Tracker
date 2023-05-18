e8b2a680cd596d5e4e0ca1b0ba95409dfb83f1e9
wasif Mohammad <wasifmohammad@wasifs-MacBook-Pro.local>
2023-05-18 21:30:49 +0600

    Second commit

--- CorporateAssets/serializers.py
+++ CorporateAssets/serializers.py
@@ -1,27 +1,27 @@
-from rest_framework import serializers, generics
+from rest_framework import serializers
 
-from .models import Company, Employee, Asset,Asset_details
+from .models import Company, Employee, Asset, Asset_details
 
 
 class Asset_detailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Asset_details
-        fields = ('id', 'name', 'slug', 'checked_out_date', 'checked_in_date', 'condition_on_checkout', 'condition_on_checking', 'employee_id', 'asset_id')
+        fields = ('name', 'slug', 'checked_out_date', 'checked_in_date', 'condition_on_checkout', 'employee_id')
 class AssetSerializer(serializers.ModelSerializer):
-    asset_details= Asset_detailsSerializer(many=True, read_only=True)
+    #asset_details= Asset_detailsSerializer(many=True, read_only=True)
 
     class Meta:
         model = Asset
         fields = ('id', 'name', 'slug', 'description',  'employee_id')
 class EmployeeSerializer(serializers.ModelSerializer):
-    asset= AssetSerializer(many=True, read_only=True)
+    #asset= AssetSerializer(many=True, read_only=True)
 
     class Meta:
         model = Employee
         fields = ('id', 'name', 'slug' 'company_id')
 
 class CompanySerializer(serializers.ModelSerializer):
-    employee= EmployeeSerializer(many=True, read_only=True)
+    #employee= EmployeeSerializer(many=True, read_only=True)
 
     class Meta:
         model = Company
